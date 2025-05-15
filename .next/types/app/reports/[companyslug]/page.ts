e47f4939791c1083a4f16d279a750e02f6b6c1a8
Joseph Alexander <jalexander@MacBookAir.lan>
2025-05-14 22:25:24 -0400

    Push final working copy from GapAnalysisForm-dev

--- .next/types/app/reports/[companyslug]/page.ts
+++ .next/types/app/reports/[companyslug]/page.ts
@@ -1,8 +1,8 @@
 // File: /Users/jalexander/Downloads/home 4/ubuntu/manus_form_rebuild/src/app/reports/[companyslug]/page.tsx
-import * as entry from '../../../../../src/app/reports/[companyslug]/page.js'
+import * as entry from '../../../../../src/app/reports/[companyslug]/page.jsx'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../../src/app/reports/[companyslug]/page.js')
+type TEntry = typeof import('../../../../../src/app/reports/[companyslug]/page.jsx')
 
 type SegmentParams<T extends Object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
