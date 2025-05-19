be468b5fb7c22cefe8ce810213c752d392705cfd
Joseph Alexander <jalexander@MacBookAir.lan>
2025-05-19 13:24:05 -0400

    Convert project to JavaScript to fix deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
+import './globals.css';
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>{children}</body>
     </html>
   );
